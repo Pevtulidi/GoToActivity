d1fc3a14d14a15559a1fcd41052251916d33538e
Tatyana Pevtulidi <tanya.pevtulidi@yandex.ru>
2021-12-09 22:42:16 +0200

    Initial commit

---
+++ app/src/main/java/space/pevtulidi/gotoactivity/ActivityTwo.java
@@ -0,0 +1,13 @@
+package space.pevtulidi.gotoactivity;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ActivityTwo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_two2);
+    }
+}
---
+++ app/src/main/java/space/pevtulidi/gotoactivity/MainActivity.java
@@ -0,0 +1,44 @@
+package space.pevtulidi.gotoactivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnActTwo;
+
+
+    @Override
+    protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnActTwo = (Button) findViewById(R.id.btnActTwo);
+        btnActTwo.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() != R.id.btnActTwo) {
+            return;
+        }
+        Intent intent = new Intent(this, ActivityTwo.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+
+
+
+}
